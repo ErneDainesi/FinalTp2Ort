dc95c43b084d1d4dec8b5d17671858276c4f283b
ErneDainesi <edainesi@fi.uba.ar>
2024-07-01 20:22:07 -0300

    implement sonda controller methods

--- controller/SondaController.js
+++ controller/SondaController.js
@@ -2,11 +2,34 @@ import SondaService from '../services/SondaService.js';
 
 const service = new SondaService();
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
+    try {
+        const {id, temperatura} = req.body;
+        const result = await service.register(id, temperatura);
+        const status = result.errorMsg ? 500 : 200;
+        res.status(status).json(result);
+    } catch (error) {
+        res.status(500).json({errorMsg: error.message})
+    }
 }
 
-export const listAll = (req, res) => {
+export const listAll = async (req, res) => {
+    try {
+        const result = await service.listAll();
+        const status = result.errorMsg ? 400 : 200;
+        res.status(status).json(result);
+    } catch (error) {
+        res.status(500).json({errorMsg: error.message})
+    }
 }
 
-export const listSingle = (req, res) => {
+export const listSingle = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await service.listSingle(id);
+        const status = result.errorMsg ? 400 : 200;
+        res.status(status).json(result);
+    } catch (error) {
+        res.status(500).json({errorMsg: error.message})
+    }
 }

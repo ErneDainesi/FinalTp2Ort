171eecec8a7434a65f5db6697fae919a7fba7ff1
ErneDainesi <edainesi@fi.uba.ar>
2024-07-01 20:22:24 -0300

    implement sonda service methods

--- services/SondaService.js
+++ services/SondaService.js
@@ -1,11 +1,44 @@
+import Sonda from "../models/Sonda.js";
+import { validateId, validateTemp } from "../utils/SondaValidator.js";
+
 class SondaService {
-    async register() {
+    sondaModel = new Sonda();
+
+    async register(id, temperatura) {
+        try {
+            if (validateId(id)) {
+                throw Error("Invalid id");
+            }
+            if (validateTemp(temperatura)) {
+                throw Error("Invalid temperature");
+            }
+            const previousSonda = await this.sondaModel.sondaExists(id);
+            if (previousSonda) {
+                throw Error("Sonda with same id already exists");
+            }
+            const data = await this.sondaModel.create({id, temperatura});
+            return {data};
+        } catch (error) {
+            return {errorMsg: error.message};
+        }
     }
 
-    async listSingle() {
+    async listSingle(id) {
+        try {
+            const data = await this.sondaModel.getById(id);
+            return {data};
+        } catch (error) {
+            return {errorMsg: error.message};
+        }
     }
 
     async listAll() {
+        try {
+            const data = await this.sondaModel.getAll();
+            return {data};
+        } catch (error) {
+            return {errorMsg: error.message};
+        }
     }
 }
 

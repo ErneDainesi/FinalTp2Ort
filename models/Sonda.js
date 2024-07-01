a61c9a4a4d142f95121e08b569d01dc98a55e388
ErneDainesi <edainesi@fi.uba.ar>
2024-07-01 20:58:45 -0300

    use array instead of map

--- models/Sonda.js
+++ models/Sonda.js
@@ -1,31 +1,25 @@
 class Sonda {
-    // Uso mapa para hacer una busqueda mas eficaz
-    sondas = new Map();
+    sondas = [];
 
     async create(id, temperatura) {
         const time = new Date();
         const timestamp = `${time.getDay()}/${time.getMonth()}/${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}`;
-        const newRegister = {id, temperatura, timestamp};
-        const parsedId = JSON.stringify(id);
-        const sonda = this.sondas.get(parsedId);
-        if (sonda) {
-            sonda.push(newRegister);
-        } else {
-            this.sondas.set(parsedId, [newRegister]);
-        }
-        return sonda ? sonda : this.sondas.get(parsedId);
+        const parsedId = typeof id === 'string' ? id : JSON.stringify(id);
+        const newRegister = {id: parsedId, temperatura, timestamp};
+        this.sondas.push(newRegister);
+        return newRegister;
     }
 
     async getAll() {
-        return Array.from(this.sondas.values());
+        return this.sondas;
     }
 
     async getById(id) {
-        const parsedId = JSON.stringify(id);
-        if (!this.sondas.has(parsedId)) {
-            throw Error("Provided sonda does not exist.");
+        const res = this.sondas.filter((register) => id === register.id);
+        if (res.length <= 0) {
+            throw Error("Provided sonda does not exist");
         }
-        return this.sondas.get(parsedId);
+        return res;
     }
 }
 
